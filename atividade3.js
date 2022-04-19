84a3787cc5552eb8cf055d51146a2cc7657bcf90
AndrezaPipolo <andreza_menezes@hotmail.com>
2022-04-18 23:35:24 -0300

    atualização de atividade

--- atividade3.js
+++ atividade3.js
@@ -1,14 +1,9 @@
-/*Faça um programa que dado um número
-imprime no (console.log) a tabuada do mesmo de 1 a 10. */
 
- function tabuada(num){
-     let stringtabuada = ""
-     for(i = 1; 1 <= 10; i++){
-     let resultado = num * i
-      stringtabuada - `${stringtabuada} ${num} x ${i} - ${resultado}\n` 
- } 
-      return stringtabuada
 
+function multiplicationTable(num){
+
+   for(x=0; x<=10 ; x++)
+   console.log(`${num} x ${x} = ${(num*x)}`)
 }
 
-   console.log(tabuada(12))
+multiplicationTable(5)
